ffd19a9f62c677e87fb055f2cfda22b765b87184
Kxrnnsiri <74240951+Kxrnnsiri@users.noreply.github.com>
2020-12-08 18:24:50 +0700

    Update inform.py

--- inform.py
+++ inform.py
@@ -6,20 +6,34 @@ def inform():
     inform.geometry('590x550')
     inform.title("BMIW")
     inform.configure(background='#660000')
+    inform.resizable(width=False,height=False)
     headder = PhotoImage(file = "You Inform.png")
     Label(inform, image=headder, borderwidth=0).grid(row=1, column=0)
     Label(inform, text="Your Weight"+"      "*6,font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=2,column=0)
-    weight = Entry(inform,font="consolas 15", bg="#FFFFFF",fg="#660000")
+    weight = Entry(inform,font="consolas 14", bg="#FFFFFF",fg="#660000")
     weight.grid(row=3,column=0)
-    Label(inform,text="*หมายเหตุหน่วยเป็นกิโลกรัม",bg="#660000",fg="#FFFFFF").grid(row=4,column=0)
+    kg = ttk.Combobox(inform,values = ['Kilogram','Pound'],font="Mali 8", state = "readonly", width=20)
+    kg.grid(row=3, column=0,sticky='E' )
+    kg.set("Choose one....")
     Label(inform, text="Your Height"+"      "*6,font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=5,column=0)
-    height = Entry(inform,font="consolas 15", bg="#FFFFFF",fg="#660000")
+    height = Entry(inform,font="consolas 14", bg="#FFFFFF",fg="#660000")
     height.grid(row=6,column=0)
+    hi = ttk.Combobox(inform,values = ['Kilogram','Pound'],font="Mali 8",state = "readonly", width=20,)
+    hi.grid(row=6, column=0, sticky='E')
+    hi.set("Choose one....")
     weight1 = weight.get()
     height1 = height.get()
-    Label(inform,text="*หมายเหตุหน่วยเป็นเซนติเมตร",bg="#660000",fg="#FFFFFF").grid(row=7,column=0)
     Label(inform, text='\n',font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=8,column=0)
-    submit = Button(inform, text="Submit",bg="#FFFFFF",fg="#660000",font ="consolas 10",command=).grid(row=8,column=0)
+    
+        def main():
+        noti = messagebox.askyesno("Check!",'Save this information?')
+        if True:
+            pass
+            ###ใส่พวก check
+        else:
+                noti.destroyed()
+                
+    submit = Button(inform, text="Submit",bg="#FFFFFF",fg="#660000",font ="consolas 10",command=main).grid(row=8,column=0)
     down = PhotoImage(file="ploo.png")
     Label(inform, image=down,borderwidth=0).grid(row=9,column=0)
     inform.mainloop()
