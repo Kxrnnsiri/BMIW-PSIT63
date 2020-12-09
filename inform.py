328f6d179a446facf0a3613e43608efd8f20596c
santhitak <74261236+santhitak@users.noreply.github.com>
2020-12-10 00:09:38 +0700

    Update inform.py

--- inform.py
+++ inform.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 def inform():
 
     inform = Tk()
-    inform.geometry('590x550')
+    inform.geometry('590x430')
     inform.title("BMIW")
     inform.configure(background='#660000')
     inform.resizable(width=False,height=False)
@@ -13,28 +13,28 @@ def inform():
     header = PhotoImage(file = "You Inform.png")
 
     Label(inform, image=header, borderwidth=0).grid(row=1, column=0)
-    Label(inform, text="Your Weight"+"      "*6,font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=2,column=0)
+    Label(inform, text="Enter Your Weight:"+"       "*6,font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=3,column=0)
 
     weight = Entry(inform,font="consolas 14", bg="#FFFFFF",fg="#660000")
-    weight.grid(row=3,column=0)
+    weight.grid(row=4,column=0)
 
     kg = ttk.Combobox(inform,values = ['Kilogram','Pound'],font="Mali 8", state = "readonly", width=20)
-    kg.grid(row=3, column=0,sticky='E' )
-    kg.set("Choose one....")
+    kg.grid(row=4, column=0,sticky='E' )
+    kg.set("units..")
 
-    Label(inform, text="Your Height"+"      "*6,font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=5,column=0)
+    Label(inform, text="Enter Your Height:"+"       "*6,font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=6,column=0)
 
     height = Entry(inform,font="consolas 14", bg="#FFFFFF",fg="#660000")
-    height.grid(row=6,column=0)
+    height.grid(row=8,column=0)
 
-    hi = ttk.Combobox(inform,values = ['Kilogram','Pound'],font="Mali 8",state = "readonly", width=20,)
-    hi.grid(row=6, column=0, sticky='E')
-    hi.set("Choose one....")
+    unit = ttk.Combobox(inform,values = ['Kilogram','Pound'],font="Mali 8",state = "readonly", width=20,)
+    unit.grid(row=8, column=0, sticky='E')
+    unit.set("units..")
 
-    weight1 = weight.get()
-    height1 = height.get()
+    weight_get = weight.get()
+    height_get = height.get()
 
-    Label(inform, text='\n',font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=8,column=0)
+    Label(inform, text='\n',font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=11,column=0)
 
     def main():
         noti = messagebox.askyesno("Check!",'Save this information?')
@@ -44,10 +44,10 @@ def inform():
     else:
         noti.destroyed()
 
-    submit = Button(inform, text="Submit",bg="#FFFFFF",fg="#660000",font ="consolas 10",command=main).grid(row=8,column=0)
+    submit = Button(inform, text="Submit",bg="#FFFFFF",fg="#660000",font ="consolas 10",command=main).grid(row=11,column=0)
     down = PhotoImage(file="ploo.png")
 
-    Label(inform, image=down,borderwidth=0).grid(row=9,column=0)
+    Label(inform, image=down,borderwidth=0).grid(row=12,column=0)
     inform.mainloop()
 
 inform()
