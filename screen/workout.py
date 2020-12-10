9b4e34e6c579aee3341ef3d30c4edd05481afe57
Juramas <63070022@kmitl.ac.th>
2020-12-10 12:14:57 +0700

    เพิ่มปุ่มรูป

--- screen/workout.py
+++ screen/workout.py
@@ -10,13 +10,52 @@ def workoutSelector():
         workoutArm(window)
 
     window = Tk()
-    window.geometry("800x500")
+    window.title('เมนูออกกำลังกาย')
+    # window.geometry("800x500")
     window.configure(background = theme.background_color)
 
     img_body = PhotoImage(master = window, file = "assets/arm.png") # ภาพแขน
 
-    btn_arm = Button(window, image = img_body, command = openArm) #ปุ่มภาพแขน
-    btn_arm.pack()
+    btn_arm = Button(
+        window,
+        image = img_body,
+        height = 100,
+        width = 100,
+        command = openArm) #ปุ่มภาพแขน
+    btn_arm.pack(padx = 10, pady = 10)
+
+    btn_ab = Button(
+        window,
+        image = img_body,
+        height = 100,
+        width = 100,
+        command = openArm) #ปุ่มภาพท้อง
+    btn_ab.pack(padx = 10, pady = 10)
+
+    btn_leg = Button(
+        window,
+        image = img_body,
+        height = 100,
+        width = 100,
+        command = openArm) #ปุ่มภาพขา
+    btn_leg.pack(padx = 10, pady = 10)
+
+    btn_bottom = Button(
+        window,
+        image = img_body,
+        height = 100,
+        width = 100,
+        command = openArm) #ปุ่มภาพก้น
+    btn_bottom.pack(padx = 10, pady = 10)
+
+    btn_full = Button(
+        window,
+        image = img_body,
+        height = 100,
+        width = 100,
+        command = openArm) #ปุ่มภาพ ทุกส่วนของร่างกาย
+    btn_full.pack(padx = 10, pady = 10)
+    # btn_arm.place(height=100, width=100)
 
     # btn_leg = Button(window, image = img_body, command = openLeg) #ปุ่มภาพแขน
     # btn_leg.pack()
