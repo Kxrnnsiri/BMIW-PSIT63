6cfc0b749719b5399b088cda6d480f7c1db12c71
santhitak <74261236+santhitak@users.noreply.github.com>
2020-12-13 03:01:36 +0700

    Update inform.py

--- inform.py
+++ inform.py
@@ -1,63 +1,66 @@
 from tkinter import*
 from tkinter import messagebox
-from tkinter import ttk
+import tkinter as tk
+import os
+# from PIL import *
 
 def inform():
 
     inform = Tk()
-    inform.geometry('590x430')
+    inform.geometry('590x550')
     inform.title("BMIW")
     inform.configure(background='#660000')
     inform.resizable(width=False,height=False)
 
-    header = PhotoImage(file = "You Inform.png")
-
+    header = PhotoImage(file = "You Inform1.png")
     Label(inform, image=header, borderwidth=0).grid(row=1, column=0)
-    Label(inform, text="Enter Your Weight:"+"       "*6,font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=3,column=0)
-
-    weight = Entry(inform,font="consolas 14", bg="#FFFFFF",fg="#660000")
-    weight.grid(row=4,column=0)
-
-    kg = ttk.Combobox(inform,values = ['Kilogram','Pound'],font="Mali 8", state = "readonly", width=20)
-    kg.grid(row=4, column=0,sticky='E' )
-    kg.set("units..")
-
-    Label(inform, text="Enter Your Height:"+"       "*6,font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=6,column=0)
-
-    height = Entry(inform,font="consolas 14", bg="#FFFFFF",fg="#660000")
-    height.grid(row=8,column=0)
-
-    unit = ttk.Combobox(inform,values = ['meter','cm'],font="Mali 8",state = "readonly", width=20,)
-    unit.grid(row=8, column=0, sticky='E')
-    unit.set("units..")
-
-    weight_get = weight.get()
-    height_get = height.get()
-
-    Label(inform, text='\n',font="Mali 12",bg="#660000",fg="#FFFFFF").grid(row=11,column=0)
-
-    def checkinput():
-        ###ฝากเเก้ตรงนี้ที มันคือcheckinputกับunit เค้าจะไปทำgoogledocต่อเเล้ว
-        if int(weight_get) != listnum or int(height_get) != listnum:
-             messagebox.showerror("Error!!!","It's must be a num")
-        else:
-            if int(weight_get) >= 500:
-                messagebox.showerror("Error!","Your weight was too much")
-            if int(height_get) >= 500:
-                messagebox.showerror("Error!", "Your height was too much")
-
-    def main():
-        noti = messagebox.askyesno("Check!",'Save this information?')
-        if True:
-            checkinput()
-        else:
-            noti.destroyed()
 
 
-    submit = Button(inform, text="Submit",bg="#FFFFFF",fg="#660000",font ="consolas 10",command=main).grid(row=11,column=0)
-    down = PhotoImage(file="ploo.png")
+    Label(inform, text='\n',font="Mali 11",bg="#660000",fg="#FFFFFF").grid(row=2,column=0)
+    Label(inform, text="Enter Your Weight (kg.) :"+"       "*6,font="Mali 11",bg="#660000",fg="#FFFFFF").grid(row=3)
+    Label(inform, text='\n',font="Mali 11",bg="#660000",fg="#FFFFFF").grid(row=3,column=0)
+    Label(inform, text="Enter Your Height (cm.) :"+"       "*6,font="Mali 11",bg="#660000",fg="#FFFFFF").grid(row=6)
+    Label(inform, text='\n',font="Mali 11",bg="#660000",fg="#FFFFFF").grid(row=6,column=0)
 
-    Label(inform, image=down,borderwidth=0).grid(row=12,column=0)
+    weight = tk.DoubleVar()
+    weight.set("")
+    height = tk.DoubleVar()
+    height.set("")
+    bmires = tk.DoubleVar()
+
+    weight_ent = tk.Entry(inform, textvariable=weight, font="consolas 12", bg="#FFFFFF",fg="#660000").grid(row=4)
+    height_ent = tk.Entry(inform, textvariable=height, font="consolas 12", bg="#FFFFFF",fg="#660000").grid(row=8)
+
+        ## ref for input entry : https://stackoverflow.com/questions/47378715/tkinter-how-to-get-the-value-of-an-entry-widget
+
+    def pagecal():
+        chart = 'chart.py'
+        os.system(chart)
+
+    def bmi_calculator():
+
+        bmi = weight.get()/((height.get()/100)**2)
+
+        resulthead = tk.Label(inform, text= 'Your bmi is',font="Mali 11",bg="#660000",fg="#FFFFFF").grid(row=13,column=0)
+        bmires = tk.Label(inform, text= bmi, font="Mali 11",bg="#660000",fg="#FFFFFF").grid(row=14,column=0)
+
+        chart = Button(inform, text="Next",bg="#FFFFFF",fg="#660000",font ="consolas 10", command=pagecal).grid(row=16,column=0)
+
+        return resulthead and bmires and chart and pagecal()
+
+    # def check():
+    #     if not weight.get() and not height.get():
+    #         messagebox.showinfo("Error", "Please enter valid data")
+
+    #     else:
+    #         return bmi_calculator()
+
+    Label(inform, text='\n',font="Mali 11",bg="#660000",fg="#FFFFFF").grid(row=10,column=0)
+
+    submit = Button(inform, text="Submit",bg="#FFFFFF",fg="#660000",font ="consolas 10", command=bmi_calculator).grid(row=11,column=0)
+
+    Label(inform, text='\n',font="Mali 10",bg="#660000",fg="#FFFFFF").grid(row=12,column=0)
+    Label(inform, text='\n',font="Mali 10",bg="#660000",fg="#FFFFFF").grid(row=15,column=0)
     inform.mainloop()
 
 inform()
