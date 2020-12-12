829f771edcba373a63e6d6ea8965c1f1c9893140
santhitak <74261236+santhitak@users.noreply.github.com>
2020-12-13 03:10:53 +0700

    Add files via upload

---
+++ workout.py
@@ -0,0 +1,19 @@
+from tkinter import*
+from tkinter import messagebox
+import tkinter as tk
+import os
+
+def works():
+
+    works = Tk()
+    works.geometry('590x350')
+    works.title("BMIW")
+    works.configure(background='#660000')
+    works.resizable(width=False,height=False)
+
+    """
+        coding area
+    """
+
+    works.mainloop()
+works()
