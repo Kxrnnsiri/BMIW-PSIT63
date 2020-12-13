3133af663d430592dc3f8de634c14deafe68e662
aumkk <74270614+aumkk@users.noreply.github.com>
2020-12-13 20:43:19 +0700

    Update workout_ab.py

--- screen/workout_ab.py
+++ screen/workout_ab.py
@@ -28,7 +28,7 @@ def workoutAb(main_window):
 
     window = Toplevel(main_window)
     window.title('เมนูออกกำลังกาย เฉพาะส่วน หน้าท้อง')
-    window.geometry('480x400')
+    window.geometry('480x320')
     window.configure(background = theme.background_color)
 
     poselink_1 = Button(
